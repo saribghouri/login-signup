d607525c337abc85f4136c07fd7e59166cabcd18
saribghouri <sarib.ghouri92@gmail.com>
2022-08-19 09:24:51 -0700

    update

--- index.mjs
+++ index.mjs
@@ -1,21 +1,68 @@
 import express from "express";
 import cors from "cors";
 import { nanoid } from "nanoid";
+import mongoose from "mongoose";
 
+let dbURI =
+  "mongodb+srv://sarib-ghouri92:445500@cluster0.nmgizsx.mongodb.net/serviceDataBase?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose is connected");
+});
+mongoose.connection.on("disconnected", function () {
+  console.log("Mongoose is disconnected");
+  process.exit(1);
+});
+
+mongoose.connection.on("error", function (err) {
+  console.log("Mongoose connection error: ", err);
+  process.exit(1);
+});
+
+process.on("SIGINT", function () {
+  console.log("app is terminating");
+  mongoose.connection.close(function () {
+    console.log("Mongoose default connection closed");
+    process.exit(0);
+  });
+});
+
+require("./model/post");
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const post = mongoose.model("post");
+
+// app.get("/signup", async (req, res) => {
+//   try {
+//     const posts = await post.find({});
+//     res.send(posts);
+//   } catch (error) {
+//     res.status(500);
+//   }
+// });
+// app.post("/login", (req, res) => {
+//   console.log(req.body);
+// });
 
 const port = process.env.PORT || 3000;
 
 let userBase = []; // TODO: replace this with mongoDB
 
-app.post("/signup", (req, res) => {
-  let body = req.body;
+app.get("/signup", async (req, res) => {
+  try {
+    const posts = await post.find({});
+    res.send(posts);
+  } catch (error) {
+    res.status(500);
+  }
 
   if (!body.firstName || !body.lastName || !body.email || !body.password) {
     res.status(400).send(
-      `required fields missing, request example: 
+      `required fields missing, request example:
                 {
                     "firstName": "John",
                     "lastName": "Doe",
@@ -55,12 +102,19 @@ app.post("/signup", (req, res) => {
   res.status(201).send({ message: "user is created" });
 });
 
-app.post("/login", (req, res) => {
-  let body = req.body;
-
+app.post("/login", async (req, res) => {
+  try {
+    const post = new post();
+    post.title = req.body.title;
+    post.content = req.body.content;
+    await post.save();
+    res.send(post);
+  } catch (error) {
+    res.status(500);
+  }
   if (!body.email || !body.password) {
     res.status(400).send(
-      `required fields missing, request example: 
+      `required fields missing, request example:
                 {
                     "email": "abc@abc.com",
                     "password": "12345"
@@ -104,10 +158,9 @@ app.post("/login", (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
