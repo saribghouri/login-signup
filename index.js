699966dd89b9b25ffc8736c2747342a849335ca6
saribghouri <sarib.ghouri92@gmail.com>
2022-08-22 07:36:26 -0700

    update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function signup() {
   }
 
   axios
-    .post("http://localhost:3000/signup", {
+    .post("https://login-signup-123.herokuapp.com//signup", {
       firstName,
       lastName,
       email,
@@ -48,54 +48,3 @@ function signup() {
         error.response.data.message;
     });
 }
-
-// function signup() {
-//   var firstName = document.getElementById("firstName").value;
-//   var lastName = document.getElementById("lastName").value;
-//   var email = document.getElementById("email").value;
-//   var password = document.getElementById("password").value;
-//   var repeatPassword = document.getElementById("confirm-password").value;
-
-//   if (password !== repeatPassword) {
-//     document.querySelector("#message").innerHTML =
-//       "Passwords do not match, please try again";
-//     return;
-//   }
-
-//   axios
-//     .post("https://login-signup-123.herokuapp.com/signup", {
-//       firstName,
-//       lastName,
-//       email,
-//       password,
-//     })
-//     .then(function (response) {
-//       document.querySelector("#message").innerHTML = response.data.message;
-//     })
-//     .catch(function (error) {
-//       document.querySelector("#message").innerHTML =
-//         error.response.data.message;
-//     });
-// }
-
-// function login(event) {
-//   event.preventDefault();
-
-//   var email = document.getElementById("email").value;
-//   var password = document.getElementById("password").value;
-
-//   axios
-//     .post("https://login-signup-123.herokuapp.com/login", {
-//       email,
-//       password,
-//     })
-//     .then(function (response) {
-//       console.log(response.data);
-//       document.querySelector("#message1").innerHTML = response.data.message;
-//     })
-//     .catch(function (error) {
-//       console.log(error.response.data);
-//       document.querySelector("#message1").innerHTML =
-//         error.response.data.message;
-//     });
-// }
