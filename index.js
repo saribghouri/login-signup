8ad3e93c3992a2717c156b5b6d6a376c18b9f295
saribghouri <sarib.ghouri92@gmail.com>
2022-08-22 08:03:00 -0700

    update

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-function getAllUser() {
-  axios
-    .get("http://localhost:3000/users")
-    .then(function (response) {
-      console.log(response.data);
-
-      response.data.map((eachUser) => {
-        document.querySelector(
-          "#allUser"
-        ).innerHTML += `${eachUser.firstName} ${eachUser.lastName} - ${eachUser.email} <br>`;
-      });
-    })
-    .catch(function (error) {
-      console.log(error.response.data);
-      document.querySelector("#message").innerHTML =
-        error.response.data.message;
-    });
-}
-getAllUser();
-
 function signup() {
   var firstName = document.getElementById("firstName").value;
   var lastName = document.getElementById("lastName").value;
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
-  var repeatPassword = document.getElementById("repeatPassword").value;
+  var repeatPassword = document.getElementById("confirm-password").value;
 
   if (password !== repeatPassword) {
     document.querySelector("#message").innerHTML =
@@ -39,12 +19,32 @@ function signup() {
       password,
     })
     .then(function (response) {
-      console.log(response.data);
       document.querySelector("#message").innerHTML = response.data.message;
     })
     .catch(function (error) {
-      console.log(error.response.data);
       document.querySelector("#message").innerHTML =
+        error.response.data.message;
+    });
+}
+
+function login(event) {
+  event.preventDefault();
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  axios
+    .post("https://login-signup-123.herokuapp.com/login", {
+      email,
+      password,
+    })
+    .then(function (response) {
+      console.log(response.data);
+      document.querySelector("#message1").innerHTML = response.data.message;
+    })
+    .catch(function (error) {
+      console.log(error.response.data);
+      document.querySelector("#message1").innerHTML =
         error.response.data.message;
     });
 }
